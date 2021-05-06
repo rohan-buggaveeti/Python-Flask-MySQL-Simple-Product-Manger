ecb3b9651007edd4f45963b9c59c52511a0ae775
rohan-buggaveeti <rohan.buggaveeti@gmail.com>
2021-05-06 17:29:14 +0530

    Added delete functionality, removed unique ID issue

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ from datetime import datetime
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rohan:Fushimi#2112@localhost/trial_db'
 
-app.config['MYSQL_USER'] = 'epyqrev' #root
-app.config['MYSQL_PASSWORD'] = 'epyq@pythontrial' #Fushimi@2112
-app.config['MYSQL_HOST'] = 'epyqrev.mysql.pythonanywhere-services.com' #127.0.0.1
-app.config['MYSQL_DB'] = 'epyqrev$epyqdb' #trial_db Tablename: trial_db.products
+app.config['MYSQL_USER'] = 'root' #root, epyqrev
+app.config['MYSQL_PASSWORD'] = 'Fushimi@2112' #Fushimi@2112, epyq@pythontrial
+app.config['MYSQL_HOST'] = '127.0.0.1' #127.0.0.1, epyqrev.mysql.pythonanywhere-services.com
+app.config['MYSQL_DB'] = 'trial_db' #trial_db Tablename: trial_db.products, epyqrev$epyqdb
 mysql = MySQL(app) 
 
-
+all_entries = []
 @app.route('/')
 def index():
     cursor = mysql.connection.cursor()
     cursor.execute(''' SELECT * FROM trial_db.products''')
     all_entries = cursor.fetchall()
+    cursor.close()
     return render_template("index.html",list = all_entries)
 @app.route('/home')
 def home():
@@ -27,15 +28,22 @@ def add():
 @app.route('/add_item', methods = ['POST', 'GET'])
 def add_item():
     if request.method == "POST":
-        pid = request.form['pid']
         pname = request.form['pname']
         cat = request.form['cat']
         qty = request.form['qty']
         price = request.form['price']
         cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO products VALUES(%s,%s,%s,%s,%s)''',(pid,pname,cat,qty,price))
+        cursor.execute(''' INSERT INTO products (PName, Pcategory, Quantity, Price) VALUES(%s,%s,%s,%s)''',(pname,cat,qty,price))
         mysql.connection.commit()
         cursor.close()
+    return redirect("/")
+@app.route('/delete')
+def delete_item():
+    pid = request.args.get('pid')
+    cursor = mysql.connection.cursor()
+    cursor.execute('''DELETE FROM products WHERE (PID = %s);''',[pid])
+    mysql.connection.commit()
+    cursor.close()
     return redirect("/")
 
 if __name__ == "__main__":
