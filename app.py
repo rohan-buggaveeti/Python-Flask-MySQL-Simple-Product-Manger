cad79164a1f2919a6623f0cb045c7f64414e565b
Rohan Buggaveeti <63273045+rohan-buggaveeti@users.noreply.github.com>
2021-05-07 09:42:41 +0530

    Update app.py
    Final commit

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ from flask_mysqldb import MySQL
 from datetime import datetime
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rohan:Fushimi#2112@localhost/trial_db'
 
-app.config['MYSQL_USER'] = 'root' #root, epyqrev
-app.config['MYSQL_PASSWORD'] = 'Fushimi@2112' #Fushimi@2112, epyq@pythontrial
-app.config['MYSQL_HOST'] = '127.0.0.1' #127.0.0.1, epyqrev.mysql.pythonanywhere-services.com
-app.config['MYSQL_DB'] = 'trial_db' #trial_db Tablename: trial_db.products, epyqrev$epyqdb
+
+app.config['MYSQL_USER'] = 'mysql_username' 
+app.config['MYSQL_PASSWORD'] = 'mysql_pwd'
+app.config['MYSQL_HOST'] = 'mysql_host_url'
+app.config['MYSQL_DB'] = 'mysql_db'
 mysql = MySQL(app) 
+
+#table schema -> PID (Primary Key, Integer, Auto-Increment - starts from 1000), PName (varchar), PCategory (varchar), Quantity (Integer), Price (Integer)
 
 all_entries = []
 @app.route('/')
 def index():
     cursor = mysql.connection.cursor()
-    cursor.execute(''' SELECT * FROM trial_db.products''')
+    cursor.execute(''' SELECT * FROM mysql_db.table_name''')
     all_entries = cursor.fetchall()
     cursor.close()
     return render_template("index.html",list = all_entries)
@@ -33,7 +35,7 @@ def add_item():
         qty = request.form['qty']
         price = request.form['price']
         cursor = mysql.connection.cursor()
-        cursor.execute(''' INSERT INTO products (PName, Pcategory, Quantity, Price) VALUES(%s,%s,%s,%s)''',(pname,cat,qty,price))
+        cursor.execute(''' INSERT INTO table_name (PName, Pcategory, Quantity, Price) VALUES(%s,%s,%s,%s)''',(pname,cat,qty,price))
         mysql.connection.commit()
         cursor.close()
     return redirect("/")
@@ -41,7 +43,7 @@ def add_item():
 def delete_item():
     pid = request.args.get('pid')
     cursor = mysql.connection.cursor()
-    cursor.execute('''DELETE FROM products WHERE (PID = %s);''',[pid])
+    cursor.execute('''DELETE FROM table_name WHERE (PID = %s);''',[pid])
     mysql.connection.commit()
     cursor.close()
     return redirect("/")
